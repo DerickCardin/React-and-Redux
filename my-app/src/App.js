eaeb60e5f5f547361f0297c218b2696892dbf0b6
Derick Cardin <derickc50@gmail.com>
2018-10-09 22:01:20 +0000

    Made Stateless Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,23 @@ import Ninjas from './ninjas';
 
 
 class App extends Component {
+  state = {
+    ninjas : [
+      {name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      {name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      {name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+      ]
+    
+  }
+  
+  
   render() {
     return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
-        <Ninjas name="Ryu" age="25" belt="black" />
-        <Ninjas name="Yoshi" age="30" belt="green" />
-      </div>
+        <Ninjas ninjas={this.state.ninjas} />
+        </div>
     );
   }
 }
