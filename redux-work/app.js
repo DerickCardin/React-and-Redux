08a9a2e2d53e53c0dfba20c8baee61b4b67da49c
Derick Cardin <derickc50@gmail.com>
2018-10-11 01:51:22 +0000

    Redux Work

---
+++ redux-work/app.js
@@ -0,0 +1,15 @@
+const { createStore } = Redux;
+
+const initState = {
+    todos: [],
+    posts: []
+}
+
+function myreducer(state = initState, action){
+     console.log(action, state);
+}
+
+const store = createStore(myreducer);
+
+const todoAction = {type: 'ADD_TODO', todo: 'buy milk'};
+store.dispatch(todoAction)
