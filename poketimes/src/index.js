bbab3ccb9db794c5f1d55bcccfa00bea22ccfdef
Derick Cardin <derickc50@gmail.com>
2018-10-11 02:18:34 +0000

    Mapped sate to props

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import rootReducer from './reducers/rootReducer'
 
-const store = createStore();
-
+const store = createStore(rootReducer);
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
