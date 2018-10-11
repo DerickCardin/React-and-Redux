05f6a743a982eb4352c39eb4c88a1a59fe9c5934
Derick Cardin <derickc50@gmail.com>
2018-10-11 02:00:36 +0000

    Sotring Subscriptions- Redux

--- redux-work/app.js
+++ redux-work/app.js
@@ -6,10 +6,29 @@ const initState = {
 }
 
 function myreducer(state = initState, action){
-     console.log(action, state);
+     if (action.type == 'ADD_TODO') {
+         return {
+        ...state,
+        todos: [...state.todos, action.todo]
+         }
+     }
 }
+
+if (action.type == 'ADD_POST') {
+         return {
+        ...state,
+        posts: [...state.posts, action.post]
+         }
+     }
+
 
 const store = createStore(myreducer);
 
-const todoAction = {type: 'ADD_TODO', todo: 'buy milk'};
-store.dispatch(todoAction)
+store.subscribe(() => {
+    console.log('state updated');
+    console.log(store.getState());
+})
+
+store.dispatch({type: 'ADD_TODO', todo: 'buy milk'});
+store.dispatch({type: 'ADD_TODO', todo: 'sleep more'});
+store.dispatch({type: 'ADD_POST', postt: 'egg hunt with yoshi'});
